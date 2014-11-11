7ff09b813218ea6430e56368db0ce163ac7eabf6
lianilychee <liani.lye@students.olin.edu>
2014-11-11 00:44:03 -0500

    rudimentary red thresholding achieved

--- opencvtest.py
+++ opencvtest.py
@@ -1,22 +1,35 @@
-import numpy as np
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+while(1):
 
-	# Capture frame-by-frame
-	ret, frame = cap.read()
+    # Take each frame
+    _, frame = cap.read()
 
-	# Operations on the frame
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	# Display resulting frame
-	cv2.imshow('frame',gray)
-	
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    # define range of blue color in HSV
+    lower_red = np.array([159,50,50])
+    upper_red = np.array([179,255,255])
 
-# When everything done, release the capture
-cap.release()
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    # mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask=mask)
+
+    # cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
 cv2.destroyAllWindows()
