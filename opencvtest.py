0f09706c136965f6a158ec22f1884463abdd4891
lianilychee <liani.lye@students.olin.edu>
2014-11-14 14:10:59 -0500

    stuff

--- opencvtest.py
+++ opencvtest.py
@@ -5,31 +5,31 @@ cap = cv2.VideoCapture(0)
 
 while(1):
 
-    # Take each frame
-    _, frame = cap.read()
+	# Take each frame
+	_, frame = cap.read()
 
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	# Convert BGR to HSV
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # define range of blue color in HSV
-    lower_red = np.array([159,50,50])
-    upper_red = np.array([179,255,255])
+	# Define filter thresholds
+	# lower_red = np.array([159,50,50])
+	lower_red = np.array([0,94,66])
+	upper_red = np.array([179,255,255])
+	# upper_red = np.array([0,100,100])
 
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+	# lower_blue = np.array([110,50,50])
+	# upper_blue = np.array([130,255,255])
 
-    # Threshold the HSV image to get only blue colors
-    # mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+	# mask = cv2.inRange(hsv, lower_blue, upper_blue)
+	mask = cv2.inRange(hsv, lower_red, upper_red)
 
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask=mask)
+	res = cv2.bitwise_and(frame,frame, mask=mask)
 
-    # cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+	cv2.imshow('frame',frame)
+	# cv2.imshow('mask',mask)
+	cv2.imshow('res',res)
+	k = cv2.waitKey(5) & 0xFF
+	if k == 27:
+		break
 
 cv2.destroyAllWindows()
