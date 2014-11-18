4f40b0da246968d28b89a375cbd69a2bc8c8ef6a
lianilychee <liani.lye@students.olin.edu>
2014-11-17 23:56:18 -0500

    generating blank image

--- contourtest.py
+++ contourtest.py
@@ -1,7 +1,10 @@
 import numpy as np
 import cv2 as cv
 
-im = cv.imread('liani.jpg') # source image
+im = cv.imread('images/liani.jpg') # source image
+height, width = im.shape[:2]
+
+blank = np.zeros((height, width, 3),np.uint8)
 
 imgray = cv.cvtColor(im,cv.COLOR_BGR2GRAY) # add gray filter
 ret,thresh = cv.threshold(imgray,127,255,0) # idk what this is...
@@ -12,4 +15,5 @@ cv.drawContours(im, contours, -1, (0,255,0), 3)
 
 # show the processed image
 cv.imshow('imgray', im)
+cv.imshow('blank', blank)
 k = cv.waitKey()
