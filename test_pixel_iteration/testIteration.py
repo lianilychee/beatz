69f70f196676fc0ff726a33893914ea089124cd9
lianilychee <liani.lye@students.olin.edu>
2014-12-02 15:11:59 -0500

    printing RGB for all pixels in image matrix

--- test_pixel_iteration/testIteration.py
+++ test_pixel_iteration/testIteration.py
@@ -4,4 +4,11 @@ im = Image.open("../images/piglet_color.jpg")
 im.show()
 pix = im.load()
 [x,y] = im.size
-print pix[100,100]
+# print pix[100,100]
+
+# Iterate through matrix of pixels
+for i in range (x):
+	# print i
+	for j in range (y):
+		# print j
+		print pix[i,j]
