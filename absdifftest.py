7dc618ec688c526e8e7a378f86ec4383af678f28
lianilychee <liani.lye@students.olin.edu>
2014-12-05 22:07:43 -0500

    wrapped code into functions

--- absdifftest.py
+++ absdifftest.py
@@ -13,50 +13,53 @@ def get_image():
 	return im
 
 
-# def get_base_case():
-''' Retrieve base case image for subsequent video capture frames to compare against. '''
+def get_base_case():
+	''' Retrieve base case image for subsequent video capture frames to compare against. '''
 
-ramp_frames = 10  # Capture multiple temporary images to allow camera to adjust to environment.
+	camera = cv.VideoCapture(0)
 
-for i in xrange(ramp_frames):
-	temp = get_image()
-print "Capturing base case image."
+	ramp_frames = 10  # Capture multiple temporary images to allow camera to adjust to environment.
 
-# camera_capture = get_image()
-camera_capture = cv.cvtColor(get_image(), cv.COLOR_BGR2GRAY)
+	for i in xrange(ramp_frames):
+		temp = get_image()
+	print "Capturing base case image."
 
-cv.imwrite("/home/liani/Documents/beatz/baseCase.jpg", camera_capture)
-del(camera) # close the camera port.
+	# camera_capture = get_image()
+	camera_capture = cv.cvtColor(get_image(), cv.COLOR_BGR2GRAY)
 
-
-# get_base_case()
-
-
-# cap = cv.VideoCapture('motionTrackingTutorial/bouncingBall.avi')
-# cap = cv.VideoCapture(capture)
-# camera
-
-count = 0
-a = []
-
-while(camera.isOpened()):
-	ret, frame = camera.read()
-
-	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-	# a.append = np.asarray(gray)
-	a = np.asarray(gray)
-	time.sleep(1)
-	print a[count]
-
-	cv.imshow('frame', gray)
-	if cv.waitKey(1) & 0xFF == ord('q'):
-		break
-
-	count = count + 1
+	cv.imwrite("/home/liani/Documents/beatz/baseCase.jpg", camera_capture)
+	del(camera) # close the camera port.
 
 
-cap.release()
-cv.destroyAllWindows()
+def stream_video():
+	''' Print matrix of each video frame. '''
+
+	# cap = cv.VideoCapture('motionTrackingTutorial/bouncingBall.avi')
+	cap = cv.VideoCapture(0)
+
+	count = 0
+	a = []
+
+	while(camera.isOpened()):
+		ret, frame = camera.read()
+
+		gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+		# a.append = np.asarray(gray)
+		a = np.asarray(gray)
+		time.sleep(1)
+		print a[count]
+
+		cv.imshow('frame', gray)
+		if cv.waitKey(1) & 0xFF == ord('q'):
+			break
+
+		count = count + 1
 
 
+	cap.release()
+	cv.destroyAllWindows()
+
+
+get_base_case()
+# stream_video()
