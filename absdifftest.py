35e54780102e4f0fc5e67941f89764bcb75b59ea
lianilychee <liani.lye@students.olin.edu>
2014-12-05 21:04:40 -0500

    capturing base case image with cv2git add absdifftest.py

--- absdifftest.py
+++ absdifftest.py
@@ -1,27 +1,42 @@
-import cv2
-import cv2.cv as cv
+import cv2 as cv
 import numpy as np
 import time
 
-# Okay, I shouldn't have to use the cv2.cv package.  I should just be able to use cv2 and call it a day.  
+camera_port = 0
+ramp_frames = 30
+camera = cv.VideoCapture(camera_port)
+
+def get_image():
+	retval, im = camera.read()
+	return im
+
+for i in xrange(ramp_frames):
+	temp = get_image()
+print "Taking image..."
+
+camera_capture = get_image()
+file = "/home/liani/Documents/beatz/testimage.jpg"
+cv.imwrite(file, camera_capture)
+del(camera)
 
 
-# CAPTURING THE BASE CASE
-cv.NamedWindow("camera",1)
-capture = cv.CaptureFromCAM(0)
-while True:
-	img = cv.QueryFrame(capture)
-	cv.ShowImage("camera",img)
-	base = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	cv.SaveImage("baseCase.jpg",base)
-	if cv.WaitKey(10) == 27:
-		break
 
-img = cv.QueryFrame(capture)
-img.imshow()
+# # CAPTURING THE BASE CASE
+# cv.NamedWindow("camera",1)
+# capture = cv.CaptureFromCAM(0)
+# while True:
+# 	img = cv.QueryFrame(capture)
+# 	cv.ShowImage("camera",img)
+# 	base = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 	cv.SaveImage("baseCase.jpg",base)
+# 	if cv.WaitKey(10) == 27:
+# 		break
+
+# img = cv.QueryFrame(capture)
+# img.imshow()
 
 # cap = cv.VideoCapture('motionTrackingTutorial/bouncingBall.avi')
-cap = cv2.VideoCapture(capture)
+cap = cv.VideoCapture(capture)
 
 count = 0
 a = []
@@ -29,7 +44,7 @@ a = []
 while(cap.isOpened()):
 	ret, frame = cap.read()
 
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
 	a.append = np.asarray(gray)
 	time.sleep(1)
@@ -43,6 +58,6 @@ while(cap.isOpened()):
 
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 
 
