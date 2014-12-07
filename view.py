c6efd3c9d991bf8917c98fcc5b6802713d8b0584
lianilychee <liani.lye@students.olin.edu>
2014-12-07 14:59:26 -0500

    showing grayscale video; instrument rects should be color instead of gray, but will resolve later

--- view.py
+++ view.py
@@ -14,29 +14,25 @@ while(True):
     ret, frame = cap.read()
 
     # # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # circle(which screen, (center coordinates), (radius), (color), )
-    # cv.circle(frame, (100,200), 63, (128,114,250), -1)
-    # cv.circle(frame, (530,200), 63, (250,128,114), -1)    
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     # rectangles(which screen, (top-left corner), (bottom-right corner), (color), size)
-    cv.rectangle(frame, (37,80), (180,150), (128,114,250), -1) # Snare
-    cv.rectangle(frame, (37,380), (180,450), (204,50,153), -1) # TomTom
-    cv.rectangle(frame, (467,80), (593,150), (250,128,114), -1) # Hi Hat
-    cv.rectangle(frame, (467,380), (593,450), (127,255,0), -1) # Bass
+    cv.rectangle(gray, (37,80), (180,150), (128,114,250), -1) # Snare
+    cv.rectangle(gray, (37,380), (180,450), (204,50,153), -1) # TomTom
+    cv.rectangle(gray, (467,80), (593,150), (250,128,114), -1) # Hi Hat
+    cv.rectangle(gray, (467,380), (593,450), (127,255,0), -1) # Bass
 
 
     font = cv.FONT_HERSHEY_SIMPLEX
     # text(which screen, (position), font type, font scale, (color), thickness, lineType)
-    cv.putText(frame, 'SNARE', (50,130), font, 1, BLACK, 2, 5)
-    cv.putText(frame, 'HI HAT', (480,130), font, 1, BLACK, 2, 5)
-    cv.putText(frame, 'TOM-TOM', (30,430), font, 1, BLACK, 2, 5)
-    cv.putText(frame, 'BASS', (490,430), font, 1, BLACK, 2, 5)
+    cv.putText(gray, 'SNARE', (50,130), font, 1, BLACK, 2, 5)
+    cv.putText(gray, 'HI HAT', (480,130), font, 1, BLACK, 2, 5)
+    cv.putText(gray, 'TOM-TOM', (30,430), font, 1, BLACK, 2, 5)
+    cv.putText(gray, 'BASS', (490,430), font, 1, BLACK, 2, 5)
 
 
     # Display the resulting frame
-    cv.imshow('Example', frame)
+    cv.imshow('Example', gray)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
