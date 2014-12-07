b3be6fec2ee3aa183d88a46c72f188d326127dd3
lianilychee <liani.lye@students.olin.edu>
2014-12-07 16:41:14 -0500

    combined check_presence() and get_difference()

--- absdifftest.py
+++ absdifftest.py
@@ -27,7 +27,7 @@ def get_base_case():
 
 	camera_capture = cv.cvtColor(get_image(), cv.COLOR_BGR2GRAY)
 
-	cv.imwrite("/home/baseCase.jpg", camera_capture)
+	cv.imwrite("baseCase.jpg", camera_capture)
 	del(camera) # close camera port.
 
 	time.sleep(3) # delay to allow user to acclimate
@@ -37,25 +37,42 @@ def get_base_case():
 	return camera_capture
 
 
-def get_difference(prev, current, lowerX, upperX):
+def get_difference(prev, current, lowerX, upperX, lowerY, upperY):
 	''' Gets the difference between baseCase.jpg each video frame. '''
 
 	diff = cv.absdiff(prev, current)
 
 	# diff[ ymin:ymax , xmin:xmax]
-	trueCount = len( np.where(diff[0:480 , lowerX:upperX]>20)[0] )
+	# trueCount = len( np.where(diff[0:480 , lowerX:upperX]>20)[0] )
+	trueCount = len( np.where(diff[lowerY:upperY , lowerX:upperX]>20)[0] )
+
 
 	return trueCount 
 
 
-def check_presence(trueCount):
+def check_presence(prev, current, lowerX, upperX, lowerY, upperY):
 	''' Within a bounded region, check if an object has appeared. '''
 
-	area = 213*480
+	diff = cv.absdiff(prev, current)
+
+	# trueCount is the number of pixels within a region that have changed from the base case.
+	trueCount = len( np.where(diff[0:480, lowerX:upperX]>0)[0] )
+
+	# print trueCount
+
+	# Calculate the area of region of interest (ROI)..
+	area = abs(lowerX-upperX) * abs(lowerY-upperY)
+
+	# Calculate the percentage of pixels chanegd within ROI.
 	percent = trueCount / area
 
-	if percent > 0:
-		print true
+	return trueCount
+	# print percent
+
+	# if percent > 0:
+	# 	print 'YES'
+	# else:
+	# 	print 'NOT YET'
 
 
 def stream_video(base_case):
@@ -68,15 +85,22 @@ def stream_video(base_case):
 
 		gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-		trueCount = get_difference(prev, gray, 0, 210)
+		# Draw the snare rectangle for testing
+		BLACK = (0, 0, 0)
+		font = cv.FONT_HERSHEY_SIMPLEX
+		cv.rectangle(gray, (37,80), (180,150), (128,114,250), -1) # Snare
+		cv.putText(gray, 'SNARE', (50,130), font, 1, BLACK, 2, 5)
 
-		print trueCount
+
+
+		print check_presence(prev, gray, 37,180, 80,150)
+
+		# print trueCount = get_difference(prev, gray, 37,180, 80,150 )
 
 		cv.imshow('frame', gray)
 		if cv.waitKey(1) & 0xFF == ord('q'):
-
-	cap.release()
-	cv.destroyAllWindows()
+			cap.release()
+			cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
