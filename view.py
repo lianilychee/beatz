d7de694c5002f5697f6d055d2d2a2480dd511323
philiciachow <philicia.chow@gmail.com>
2014-12-07 23:16:24 -0500

    flipped video image

--- view.py
+++ view.py
@@ -11,41 +11,48 @@ BLACK = (0, 0, 0)
 cap = cv2.VideoCapture(0)
 
 # Sounds
-snare = pyglet.media.load('audio/snare.wav', streaming=False)
-hat = pyglet.media.load('audio/hat.wav', streaming=False)
-tom = pyglet.media.load('audio/tom.wav', streaming=False)
-bass = pyglet.media.load('audio/bass.wav', streaming=False)
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # rectangles(which screen, (top-left corner), (bottom-right corner), (color), size)
-    cv2.rectangle(gray, (37,80), (180,150), (128,114,250), -1) # Snare
-    cv2.rectangle(gray, (37,380), (180,450), (204,50,153), -1) # TomTom
-    cv2.rectangle(gray, (467,80), (593,150), (250,128,114), -1) # Hi Hat
-    cv2.rectangle(gray, (467,380), (593,450), (127,255,0), -1) # Bass
+# snare = pyglet.media.load('audio/snare.wav', streaming=False)
+# hat = pyglet.media.load('audio/hat.wav', streaming=False)
+# tom = pyglet.media.load('audio/tom.wav', streaming=False)
+# bass = pyglet.media.load('audio/bass.wav', streaming=False)
 
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    # text(which screen, (position), font type, font scale, (color), thickness, lineType)
+def main():
 
-    cv2.putText(gray, 'SNARE', (50,130), font, 1, BLACK, 2, 5)
-    cv2.putText(gray, 'HI HAT', (480,130), font, 1, BLACK, 2, 5)
-    cv2.putText(gray, 'TOM-TOM', (30,430), font, 1, BLACK, 2, 5)
-    cv2.putText(gray, 'BASS', (490,430), font, 1, BLACK, 2, 5)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Display the resulting frame
-    cv2.imshow('Example', gray)
+        frame = cv2.flip(frame, 1)
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        # rectangles(which screen, (top-left corner), (bottom-right corner), (color), size)
+        cv2.rectangle(frame, (37,80), (180,150), (128,114,250), -1) # Snare
+        cv2.rectangle(frame, (37,380), (180,450), (204,50,153), -1) # TomTom
+        cv2.rectangle(frame, (467,80), (593,150), (250,128,114), -1) # Hi Hat
+        cv2.rectangle(frame, (467,380), (593,450), (127,255,0), -1) # Bass
 
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        # text(which screen, (position), font type, font scale, (color), thickness, lineType)
+
+        cv2.putText(frame, 'SNARE', (50,130), font, 1, BLACK, 2, 5)
+        cv2.putText(frame, 'HI HAT', (480,130), font, 1, BLACK, 2, 5)
+        cv2.putText(frame, 'TOM-TOM', (30,430), font, 1, BLACK, 2, 5)
+        cv2.putText(frame, 'BASS', (490,430), font, 1, BLACK, 2, 5)
+
+        # Display the resulting frame
+        cv2.imshow('Example', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
